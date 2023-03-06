58ec6b9a2d7198458ff37610124c6b30eb42d361
unknown <mehdiferjani7@gmail.com>
2023-03-06 16:57:59 +0100

    dropped image border

--- src/pages/PreviewPage.vue
+++ src/pages/PreviewPage.vue
@@ -1,23 +1,23 @@
 <template>
   <base-layout page-title="All Files">
     <div class="flex-container">
-      <div :key="index"  v-for="(item, index) in signatures" class="card-categories ion-activatable ripple-parent" style="background:#e2e2e2;">
+      <div :key="index"  v-for="(item, index) in signatures" class="card-categories" style="background:#e2e2e2;">
         <ion-thumbnail>
-          <img :src="item.imageData" :draggable="true" v-on:dragstart="onDragStart($event, item.imageData)" v-on:dragend="onDragEnd($event)" />
+          <img :src="item.imageData" :draggable="true" v-on:dragstart="onDragStart($event)"  v-bind:data-image="item.imageData" v-on:dragend="onDragEnd($event)" />
         </ion-thumbnail>
       </div>
     </div>
     <div class="pdf-header">
-    <ion-button size="large">sign</ion-button>
     <ion-button size="large" @click="goToNextPage">Next</ion-button>
     <ion-button size="large" @click="goToPrevPage">Prev</ion-button>
+    <ion-button size="large" @click="goToPrevPage">FreeHand</ion-button>
     </div>
     <div class="pdf-container">
       <div v-if="totalPages > 0">
       Page {{ currentPage }} / {{ totalPages }}
     </div>
-      <vue-pdf-embed :source="pdfUrl" :page="currentPage" ref="pageRef" @loaded="onPdfLoaded" @touchmove.prevent @touchend.prevent @touchcancel.prevent @touchstart.prevent @drop="onDrop($event)" @dragover.prevent />
-      <canvas v-if="signatureImg" ref="canvasRef" style="position: absolute; left: 0; top: 0;"></canvas>
+    <ion-button size="large" @click="goToPrevPage"><ion-icon slot="icon-only" :icon="pencil"></ion-icon></ion-button>
+      <vue-pdf-embed :source="pdfUrl" :page="currentPage" ref="pageRef" @loaded="onPdfLoaded"  @drop="onDrop($event)" @dragover.prevent />
     </div>
   </base-layout>
 </template>
@@ -27,56 +27,133 @@
   import { usePdfStore } from "../store/PdfStore";
   import { useRoute } from "vue-router";
   import VuePdfEmbed , {VuePdfEmbedData ,VuePdfEmbedMethods} from "vue-pdf-embed";
-  import { IonButton } from "@ionic/vue";
+  import { pencil} from "ionicons/icons";
+  import { IonButton ,IonThumbnail} from "@ionic/vue";
   import { useSignatureStore } from '@/store/SignatureStore';
   export default defineComponent({
     components: {
       IonButton,
       VuePdfEmbed,
-    
+      IonThumbnail,
     },
  
     data() {
       return {
         signatureImg : null as any,
         signatures : [] as Signature[],
+        droppedSignature:[] as  any,
         pdfUrl: "",
         showPdfViewer: false,
         signedPdfUrl: "",
         currentPage: 1,
         totalPages: 0,
         loading: true,
+        pencil,
+        
       };
     },
   
     methods: {
-      onDragStart(event : any, imageData : any) {
+      onDragStart(event : any) {
+        const target = event.target as HTMLImageElement;
+        const imageData = target.getAttribute('data-image');
         event.dataTransfer.setData('text/plain', imageData);
         event.target.classList.add('dragging');
       },
       onDragEnd(event:any) {
         event.target.classList.remove('dragging');
       },
-      onDrop(event :any ) {
-       event.preventDefault();
-       const signatureData = event.dataTransfer.getData("text/plain");
-       if (!this.signatureImg) {
-       this.signatureImg = new Image();
-       const annotationLayer = document.getElementsByClassName("annotationLayer")[0];
-       annotationLayer.appendChild(this.signatureImg);
-       this.signatureImg.style.position = "absolute";
-   }
+      onDrop(event: any) {
+  event.preventDefault();
 
-  this.signatureImg.src = signatureData;
-  const canvas = document.getElementsByTagName("canvas")[0];
-  const canvasRect = canvas.getBoundingClientRect();
-  const signatureWidth = this.signatureImg.width;
-  const signatureHeight = this.signatureImg.height;
-  const x = event.offsetX - (signatureWidth / 2);
-  const y = event.offsetY - (signatureHeight / 2);
-  this.signatureImg.style.left = `${x}px`;
-  this.signatureImg.style.top = `${y}px`;
+  // Get signature data from drag event
+  const signatureData = event.dataTransfer.getData("text/plain");
+
+  const signatureImg = new Image();
+  signatureImg.src = signatureData;
+
+  signatureImg.style.width = '100%';
+  signatureImg.style.height = '100%';
+  signatureImg.style.objectFit = 'contain';
+  // Create signature container element
+  const signatureContainer = document.createElement('div');
+  signatureContainer.style.position = 'absolute';
+  signatureContainer.style.left = `${event.offsetX - (signatureImg.width)/2}px`;
+  signatureContainer.style.top = `${event.offsetY - (signatureImg.height)/2}px`;
+  signatureContainer.style.border = '1px dashed black';
+  signatureContainer.style.cursor = 'move';
+  const annotationLayer = document.getElementsByClassName("annotationLayer")[0];
+  annotationLayer.appendChild(signatureContainer);
+  // Create signature image element
+  signatureContainer.appendChild(signatureImg);
+  // Create resize handle element
+  const resizeHandle = document.createElement('div');
+  resizeHandle.style.position = 'absolute';
+  resizeHandle.style.width = '10px';
+  resizeHandle.style.height = '10px';
+  resizeHandle.style.border = '1px solid black';
+  resizeHandle.style.right = '0';
+  resizeHandle.style.bottom = '0';
+  resizeHandle.style.cursor = 'nwse-resize';
+  signatureContainer.appendChild(resizeHandle);
+
+  // Create delete button element
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'X';
+  deleteButton.style.color ='red'
+  deleteButton.style.position = 'absolute';
+  deleteButton.style.right = '0';
+  deleteButton.style.top = '0';
+  deleteButton.style.border = '1px solid red' ;
+  deleteButton.style.background ='none';
+  deleteButton.style.cursor = 'pointer';
+  signatureContainer.appendChild(deleteButton);
+
+  
+  signatureContainer.setAttribute('data-image', signatureData);
+
+
+  signatureImg.removeEventListener('dragstart', this.onDragStart);
+
+
+  signatureContainer.addEventListener('dragend', () => {
+    signatureContainer.remove();
+  });
+
+
+  resizeHandle.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+
+    const initialWidth =  signatureContainer.offsetWidth;
+    const initialHeight = signatureContainer.offsetHeight;
+    const initialX = event.clientX;
+    const initialY = event.clientY;
+
+    // Add mousemove event listener to handle resize
+    document.addEventListener('mousemove', handleResize);
+
+    // Add mouseup event listener to stop resizing
+    document.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', handleResize);
+
+    });
+
+    // Event listener function to handle resize
+    function handleResize(event : any) {
+      event.preventDefault();
+      const newWidth = initialWidth + (event.clientX - initialX);
+      const newHeight = initialHeight + (event.clientY - initialY);
+      signatureContainer.style.width = `${newWidth}px`;
+      signatureContainer.style.height = `${newHeight}px`;
+    }
+  });
+
+  // Add click event listener to delete button
+  deleteButton.addEventListener('click', () => {
+    signatureContainer.remove();
+  });
 }
+
 ,
       goToNextPage() {
         if (this.currentPage < this.totalPages) {
@@ -105,7 +182,6 @@
       this.pdfUrl = `data:application/pdf;base64,${base64String}`;
       document.body.onresize=()=>{
       (this.$refs.pageRef as VuePdfEmbedMethods)?.render()
-     // this.signatureImg=null;
 }
     },
   });
@@ -128,19 +204,13 @@
     width: 110px;
     margin-left:10px;
     opacity: 0.8;
+    cursor: move ;
   }
   
-  .card-categories {
-  background-color:  #dedede;
-  width: 100px;
-  margin: 10px;
-  text-align: center;
-  line-height: 75px;
-  font-size: 30px;
-  }
   .dragging {
     opacity: 0.7;
     transform: scale(1.2);
+    background-color: rgba(77, 151, 80, 0.404);
   }
   .pdf-container {
   background-color: #dedede;
